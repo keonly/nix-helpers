eab157de7e9dcd48533555adba556d4033e03f29
keonly <70371393+keonly@users.noreply.github.com>
2025-05-12 11:29:59 +0900

    fix(path): update path utils

--- lib/default.nix
+++ lib/default.nix
@@ -1,6 +1,6 @@
 {lib, ...}: {
   attrsets = import ./utils/attrsets.nix {inherit lib;};
-  filesystem = import ./utils/filesystem.nix {inherit lib;};
+  path = import ./utils/path.nix {inherit lib;};
 
   homeManagerBlock = import ./systems/home-manager-block.nix;
   darwinSystem = import ./systems/darwin-system.nix;
---
+++ lib/utils/path.nix
@@ -0,0 +1,12 @@
+{lib, ...}: {
+  relativeToRoot = lib.path.append ../.;
+  collectImports = path:
+    path
+    |> builtins.readDir
+    |> lib.attrsets.filterAttrs (name: _type:
+      _type
+      == "directory"
+      || (name != "default.nix" && lib.strings.hasSuffix ".nix" name))
+    |> builtins.attrNames
+    |> builtins.map (name: "${path}/${name}");
+}
