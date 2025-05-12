e0a06d87922b857b3b719408651a4262ecbb89d9
keonly <70371393+keonly@users.noreply.github.com>
2025-05-12 18:40:34 +0900

    feat(path): add path utils

--- flake.nix
+++ flake.nix
@@ -5,6 +5,10 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixpkgs-lib.url = "github:nix-community/nixpkgs.lib";
     flake-utils.url = "github:numtide/flake-utils";
+    haumea = {
+      url = "github:nix-community/haumea";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
@@ -12,9 +16,13 @@
     nixpkgs,
     nixpkgs-lib,
     flake-utils,
+    haumea,
   } @ inputs:
     {
-      lib = import ./lib {inherit (nixpkgs-lib) lib;};
+      lib = import ./lib {
+        inherit (nixpkgs-lib) lib;
+        inherit haumea;
+      };
     }
     // flake-utils.lib.eachDefaultSystem (system: {formatter = nixpkgs.legacyPackages.${system}.alejandra;});
 }
