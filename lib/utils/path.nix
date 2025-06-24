35e99e55fb065714ac969bf95c1aaa836fa2a802
keonly <keonly.git.stray407@passmail.net>
2025-06-24 11:25:14 +0900

    fix(path): proper interpolation in collectImports

--- lib/utils/path.nix
+++ lib/utils/path.nix
@@ -1,12 +1,23 @@
 {lib, ...}: {
   relativeToRoot = lib.path.append ../../.;
-  collectImports = path:
-    path
+
+  collectImports = dir: let
+    dirPath =
+      if builtins.typeOf dir == "path"
+      then dir
+      else if builtins.isString dir
+      then
+        if lib.strings.hasPrefix "/" dir
+        then builtins.path {path = dir;}
+        else throw "collectImports: Relative string paths are not supported; use a path literal or absolute path."
+      else throw "collectImports: Expected a path or string, got ${builtins.typeOf dir}: ${dir}";
+  in
+    dirPath
     |> builtins.readDir
     |> lib.attrsets.filterAttrs (name: _type:
       _type
       == "directory"
       || (name != "default.nix" && lib.strings.hasSuffix ".nix" name))
     |> builtins.attrNames
-    |> builtins.map (name: "${path}/${name}");
+    |> builtins.map (n: dirPath + "/${n}");
 }
